d55f34cbf5cbfd2a732f9a09bbba2e41ceae7e9c
ultramenid <malichamdan@gmail.com>
2024-03-22 01:49:35 +0700

    change name component

--- app/View/Components/checkbox.php
+++ app/View/Components/checkbox.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class checkbox extends Component
+class Checkbox extends Component
 {
     public $idAttribute;
     public $layer;
