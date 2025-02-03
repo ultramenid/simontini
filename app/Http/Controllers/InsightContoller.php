6a804996a6cf4a774ca951c2f618827f5e00f37f
ultramenid <malichamdan@gmail.com>
2025-02-04 00:25:02 +0700

    fix text

--- app/Http/Controllers/InsightContoller.php
+++ app/Http/Controllers/InsightContoller.php
@@ -14,12 +14,12 @@ class InsightContoller extends Controller
         return view('frontends.insight', compact('title', 'description', 'nav'));
     }
     public function stadi2024(){
-        $title = 'Status deforestasi Indonesia 2024 - Simontini';
+        $title = 'Status deforestasi Indonesia 2024 ';
         $description = 'Tahun lalu Auriga merilis data deforestasi 2023 pada Maret. Mulai tahun ini, deforestasi tahunan akan dirilis setiap Januari.';
         return view('frontends.stadi2024', compact('title', 'description'));
     }
     public function stadi2024EN(){
-        $title = 'Status of deforestation in Indonesia 2024 - Simontini';
+        $title = 'Status of deforestation in Indonesia 2024 ';
         $description = 'Auriga released deforestation data for 2023 in March last year. Commencing this year, it will release annual deforestation data each January.';
         return view('frontends.stadi2024EN', compact('title', 'description'));
     }
