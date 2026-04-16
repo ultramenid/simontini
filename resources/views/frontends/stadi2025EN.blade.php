77f9c5006972518934504a68f37d7829128043e2
ultramenid <malichamdan@gmail.com>
2026-04-17 00:08:13 +0700

    text

--- resources/views/frontends/stadi2025EN.blade.php
+++ resources/views/frontends/stadi2025EN.blade.php
@@ -1088,7 +1088,7 @@
 
                 <p class="text-black text-[50px] md:text-[20px] leading-snug font-extralight italic ">
                     Of 433,751 hectares of deforestation area, 166,590 hectares are above 5 hectares deforestation spots. 62% of deforestation occurred on spots smaller than 5 hectares, with 149,159 hectares occurring on 1 to 5 hectare spots.
-                    <span class="font-extralight pl-4" style="font-size: 3.5rem; line-height: 0; vertical-align: -0.3em;">&rdquo;</span>
+                    <span class="font-extralight pl-1" style="font-size: 3.5rem; line-height: 0; vertical-align: -0.3em;">&rdquo;</span>
                 </p>
             </div>
         </section>
