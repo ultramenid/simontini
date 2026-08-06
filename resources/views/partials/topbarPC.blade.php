48212b7cd15d91a5af845c67f99a1e2852915bb4
ultramenid <malichamdan@gmail.com>
2026-08-06 21:44:34 +0700

    change route url deforestory

--- resources/views/partials/topbarPC.blade.php
+++ resources/views/partials/topbarPC.blade.php
@@ -25,7 +25,7 @@
                 <a href="{{ route('insight', app()->getLocale()) }}" class="font-normal ">INSIGHT</a>
             </div>
 
-            <div class="py-2 hover:border-b hover:border-simontini @if($nav == 'deforestation-story') border-b border-simontini @endif">
+            <div class="py-2 hover:border-b hover:border-simontini @if($nav == 'deforestory') border-b border-simontini @endif">
                 <a href="{{ route('deforestation.index', ['locale' => app()->getLocale()]) }}" class="font-normal">DEFORESTORY</a>
             </div>
 
